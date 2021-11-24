2b101c07950422ff5bc12e2e60050de98b18c058
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2021-11-24 22:07:45 +0600

    Profile edit

--- view/profile.php
+++ view/profile.php
@@ -4,9 +4,10 @@ if (isset($_COOKIE['flag'])) {
     require("../control/db.php");
     // $Username = $_SESSION['username'];
     // $query = "SELECT * FROM users where user_username='Saidul_Khan'";
-    $query = "SELECT * FROM users where user_username='Saidul_Khan'";
+    $query = "SELECT * FROM users where username='Saidul_Khan'";
     $query_run = mysqli_query($connection, $query);
 
+    // check if profile exists
     if (mysqli_num_rows($query_run) > 0) {
         $row = mysqli_fetch_assoc($query_run);
     }
@@ -30,72 +31,9 @@ if (isset($_COOKIE['flag'])) {
         <link rel="stylesheet" href="./styles/banner.css">
         <link rel="stylesheet" href="./styles/footer.css">
         <link rel="stylesheet" href="./styles/text-animation.css">
+        <link rel="stylesheet" href="./styles/profile.css">
         <title>Profile</title>
-        <style>
-            * {
-                font-family: 'Poppins', sans-serif;
-            }
 
-            .profile {
-                margin: 0 auto;
-                padding: 30px;
-                width: 35%;
-                border: 2px solid blueviolet;
-                border-radius: 10px;
-                background: #000000;
-                background: -webkit-linear-gradient(to left, #434343, #000000);
-                background: linear-gradient(to left, #434343, #000000);
-                color: white;
-                margin-top: 100px;
-                height: auto;
-                transition: all 0.3s ease-out;
-                line-height: 2;
-                font-size: 105%;
-
-            }
-
-            hr {
-                border: 1px solid blueviolet;
-            }
-
-            p,
-            strong,
-            .far,
-            h1 {
-                color: white;
-            }
-
-            .button-62 {
-                background: linear-gradient(to bottom right, #EF4765, #FF9A5A);
-                border: 0;
-                border-radius: 12px;
-                color: #FFFFFF;
-                cursor: pointer;
-                display: inline-block;
-                font-family: -apple-system, system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
-                font-size: 16px;
-                font-weight: 500;
-                line-height: 2.5;
-                outline: transparent;
-                padding: 0 1rem;
-                text-align: center;
-                text-decoration: none;
-                transition: box-shadow .2s ease-in-out;
-                user-select: none;
-                -webkit-user-select: none;
-                touch-action: manipulation;
-                white-space: nowrap;
-                margin: 25px 10px 15px 10px;
-            }
-
-            .button-62:not([disabled]):focus {
-                box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
-            }
-
-            .button-62:not([disabled]):hover {
-                box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
-            }
-        </style>
     </head>
 
     <body>
@@ -111,25 +49,33 @@ if (isset($_COOKIE['flag'])) {
                     <h1 style="color:cyan; margin-bottom:15px;">[ PROFILE ]</h1>
                 </center>
                 <center>
-                    <img src="./images/users-profile-picture/user-1.png" alt="" width="45%" height="auto" style="border: 2px solid blueviolet; border-radius:50%;">
+                    <img src="<?php echo $row['photo']; ?>" alt="" width="45%" height="auto" style="border: 2px solid blueviolet; border-radius:50%;">
                     <br>
-                    <h1><?php echo $row['user_fname']; ?></h1>
+                    <h1>
+                        <?php
+                        // echo $row['user_fname']; 
+                        ?>
+                    </h1>
                 </center>
                 <br>
                 <br>
-                <p><strong><i class="far fa-user-circle"></i>&nbsp;&nbsp;&nbsp;Username:</strong>&nbsp;<?php echo $row['user_username']; ?></p>
+                <p><strong><i class="far fa-user-circle"></i>&nbsp;&nbsp;&nbsp;Username:</strong>&nbsp;<?php echo $row['username']; ?></p>
                 <!-- <br> -->
-                <p><strong><i class="fas fa-lock"></i>&nbsp;&nbsp;&nbsp;Password:</strong>&nbsp;<?php echo $row['user_password']; ?>
+                <p><strong><i class="fas fa-lock"></i>&nbsp;&nbsp;&nbsp;Password:</strong>&nbsp;<?php echo $row['password']; ?>
                 </p>
                 <!-- <br> -->
-                <p><strong><i class="fas fa-at"></i>&nbsp;&nbsp;&nbsp;Email:</strong>&nbsp;<?php echo $row['user_email']; ?>
+                <p><strong><i class="fas fa-at"></i>&nbsp;&nbsp;&nbsp;Email:</strong>&nbsp;<?php echo $row['email']; ?>
                 </p>
                 <!-- <br> -->
-                <p><strong><i class="fas fa-venus-mars"></i>&nbsp;&nbsp;&nbsp;Gender:</strong>&nbsp;<?php echo $row['user_gender']; ?>
-                </p>
+                <!-- <p><strong><i class="fas fa-venus-mars"></i>&nbsp;&nbsp;&nbsp;Gender:</strong>&nbsp;<?php
+                                                                                                            // echo $row['user_gender']; 
+                                                                                                            ?>
+                </p> -->
                 <!-- <br> -->
-                <p><strong><i class="fas fa-baby"></i>&nbsp;&nbsp;&nbsp;Date of
-                        Birth:</strong>&nbsp;<?php echo $row['user_dob']; ?></p>
+                <!-- <p><strong><i class="fas fa-baby"></i>&nbsp;&nbsp;&nbsp;Date of
+                        Birth:</strong>&nbsp;<?php
+                                                // echo $row['user_dob']; 
+                                                ?></p> -->
                 <!-- <br> -->
                 <!-- <p><strong><i class="fas fa-house-user"></i>&nbsp;&nbsp;&nbsp;Address:</strong>&nbsp;383/1 Free School Street, Dhanmondi, Dhaka-1205</p> -->
                 <!-- <br> -->
